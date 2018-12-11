73b6adbf8133cad365cc194463fa816662bf381e
YireKim <dlfp2004@gmail.com>
2018-12-11 16:41:06 +0900

    firs commit

---
+++ Arithmetic/Sequence.java
@@ -0,0 +1,21 @@
+package Arithmetic;
+/**
+ * 등차수열 : 연속하는 두 항의 차이가 모두 일정한 수열
+ * 공차수열 : 두 항들에 대해서 공통적으로 나타나는 차
+ *
+ */
+public class Sequence {
+	public static void main(String[] args) {
+		
+		int a = 2;
+		int sum = 0;
+		int sum2 = 6;
+		String res = "";
+
+		for(int i = 0; i < 5; i++) {
+			sum += 6;
+		}
+		System.out.println(res+" "+sum);
+	}
+}
+//2 8 14 20 26 = 70
